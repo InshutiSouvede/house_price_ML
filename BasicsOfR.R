84331304ebe1ea4981af2229719da829fdbf62fa
Souvede Inshuti <isouvede2@gmail.com>
2024-03-12 06:27:21 -0700

    Revised contents and removed confusion

--- BasicsOfR.R
+++ BasicsOfR.R
@@ -1,63 +1,88 @@
 print("Hello world")
-browseURL("http://cran.r-project.org/web/views/")
+x<-10
+
+#Printing variables using paste()
+paste("The number I was hiding is",x)
+# to browse a certain page 
+# browseURL("http://cran.r-project.org/web/views/")
+
 #to install packages,use this way for others to have them when you share your codes
-#install.packages("LiblineaR")
+#install.packages()
 # to run one line only use: ctl+C+enter
 # to comment a line use: ctrl+shift+C
-library()
+#To get load packages:
+#library() or require()
+require(LiblineaR)
+
+# To get the list of all the attached packages in R search path:
 search()
 
-require(LiblineaR)
+
 # to remove library from user libraties:
 # remove.packages(LiblineaR)
-?? igraph
-# to get all R embedded dataset:
+
+# to get all R embedded datasets (You can use them for practice):
 data()
+
+#To get information on a package:
 library(help="datasets")
-?? iris
-# to view contents of 'iris'dataset:
-iris
+
+# to view contents of 'iris' an embedded dataset:
+View(iris)
 # To load this dataset in your workspace:
 data("iris")
+
+# Variab;e value assignment:
 x <- 1:10
 x2 <- seq(5,50, by = 5)
 x2
 # To inter data into list x3 manually:
-x3 <- scan()
+x3 <- scan() # to stop it press enter without putting a value
 x3
 
-# Importing data from txt file()
-txtReport <- read.table("/home/souvede/Documents/R_course/sometxt.txt", header=TRUE, sep = " ")
+# Importing data from a file (read.table and read.csv)
+txtReport <- read.table("C:/Users/LENOVO/Documents/R/house_price_ML/sometxt.txt", header=TRUE, sep = " ")
+# View the table titles as string in console
 str(txtReport)
-csvReport <- read.csv("/home/souvede/Documents/R_course/Report.csv", header=TRUE)
+csvReport <- read.csv("C:/Users/LENOVO/Documents/R/house_price_ML/Report.csv", header=TRUE)
 View(csvReport)
 # Example 2
-Product <- read.table("/home/souvede/Documents/R_course/Product.txt", header=TRUE, sep = " ")
-str(txtReport)
-Customer <- read.csv("/home/souvede/Documents/R_course/Customer.csv", header=TRUE)
+Product <- read.table("C:/Users/LENOVO/Documents/R/house_price_ML/Product.txt", header=TRUE, sep = ',')
+str(Product)
+Customer <- read.csv("C:/Users/LENOVO/Documents/R/house_price_ML/Customer.csv", header=TRUE)
 View(Customer)
+
 # Using the data from the files(Regions column and frequency of its data)
-y <- table(Customer$Region)
+y <- table(Customer$Region) # the table() makes a two column table of value and frequency making it eay to plot
 View(y)
-# To plot y in  a barchart
+
+# To plot y in  a bar-chart
 barplot(y)
-# Arrang in order of height
+
+# Arrange in order of height
 barplot(y[order(y)])#to order in descending order, use y[order(-y)]
+
 # Change orientation to horizontal
 barplot(y[order(y)],horiz = TRUE)
-# Chanfe color of barplot
+
+# Change color of barplot
 barplot(y[order(y)], col= c("violet","blue","orange","green"))
+
 # List all colors in R
 colors()
-#To remove borders on the bars
+
+#To remove borders on the bars: border = NA
 barplot(y[order(y)], col= c("violet","beige","orange","green"), border = NA)
-# To add a title
+
+# To add a title: main = "My Title"
 barplot(y[order(y)], col= c("violet","beige","orange","green"), border = NA, main = "Frequency of Regions")
-# To label x axis
+
+# To label x axis: xlab = "x-axis"
 barplot(y[order(y)], col= c("violet","beige","orange","green"), border = NA, main = "Frequency of Regions", ylab = "Number of customers", xlab = "Region")
-# To export the graph as an image:
+
+# To export the graph as an image:(in codes instead of using the studio)
 # 1:
-png(filename = "/home/souvede/Documents/R_course/frequency.png", width= 888, height = 571 )
+png(filename = "C:/Users/LENOVO/Documents/R/house_price_ML/frequency.png", width= 888, height = 571 )
 # 2:
 barplot(y[order(y)], col= c("violet","blue","orange","green"), border = NA, main = "Frequency of Regions", ylab = "Number of customers", xlab = "Region")
 # 3: to switch of the graph device
@@ -66,7 +91,9 @@ dev.off()
 
 # Creating a histogram
 hist(Customer$Age, col = "blue")
-# To change file packetsi. number of bars
+
+# To change number of bars
 hist(Customer$Age, col = "blue", breaks = 7)
+
 #You can specify the range of bars
-hist(Customer$Age, col = "blue", breaks = c(0,40,60,100),freq = TRUE, main = "Histogram of age", xlab = "Age")
+hist(Customer$Age, col = "blue", breaks = c(0,40,60,80),freq = TRUE, main = "Histogram of age", xlab = "Age")
